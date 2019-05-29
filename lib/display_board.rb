dc676c2afba31da41541667fa1fc52e9c3ba9ec1
Benjamin Cohn <cohnbj13@gmail.com>
2019-05-29 22:48:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,8 @@
-def display_borad
+def display_board
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
-end# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+end
+# Define a method display_board that prints a 3x3 Tic Tac Toe Board
